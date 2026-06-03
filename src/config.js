9bfd1c7aac680d16f5200aadedd903120ffa8248
akash <akashks304@gmail.com>
2026-06-03 15:36:27 +0530

    chore: update deployed api url to latest version

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbypINxVfJqajcEU7VGIFzVtSyt6hOYeHgLh3ftUlXSgZzhXDCBsBV-fOdwQf4XxHNA/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbxYB1ovVvytqNpunuN_JpenaFx-HamIkCfbdJf4h0CZwutPumt-KhA0ag_KSN9juyVQnw/exec";
 export const EMPLOYEES = ["Akash", "Febin", "Sajini"];
 export const GlobalCache = {};
