12408a9654299faba67d023235f437d1ffc361d7
akash <akashks304@gmail.com>
2026-06-05 16:51:38 +0530

    fix: correct typo in Apps Script URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbKiaVDNOdOe765XsO9qxf-ScZY5jPbVMMBvVI4UdxkSWM4u8arz7Yodfqw5xro1aNkCA/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwKiaVDNOdOe765XsO9qxf-ScZY5jPbVMMBvVI4UdxkSWM4u8arz7Yodfqw5xro1aNkCA/exec";
 export const EMPLOYEES = ["Akash", "Febin", "Sajini"];
 export const GlobalCache = {};
